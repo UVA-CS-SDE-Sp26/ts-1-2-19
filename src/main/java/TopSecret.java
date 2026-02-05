182353db7afafd3ecb1602eb56c475ab27ae7548
Noodlebirb <ethanvolcanic@gmail.com>
2026-02-04 23:29:05 -0500

    Fixed issues with TopSecret (missing curly brace and no main method)

--- src/main/java/TopSecret.java
+++ src/main/java/TopSecret.java
@@ -12,30 +12,34 @@ import java.util.List;
  * Commmand Line Utility
  */
 public class TopSecret {
-    public void accessFiles(String[] args){         //this method attempts to access files from the data folder
+    public static void accessFiles(String[] args) {         //this method attempts to access files from the data folder
 
         List<String> files = new ArrayList<>();      //create list of files.
         Path dataPath = Paths.get("data");      //path to the files; I created a new folder under TopSecret with random files a.txt and b.txt
 
-        if (args.length == 0){ //if arguments = 0
+        if (args.length == 0) { //if arguments = 0
             int counter = 0;
             // using try catch for stuff involving file systems.
             try (DirectoryStream<Path> stream = Files.newDirectoryStream(dataPath)) { //opens the system for files
                 for (Path aFile : stream) {  //for every file in the stream
                     files.add(aFile.getFileName().toString()); //converts the filename to string and adds it into the prepared arraylist
-                    if (counter < 10) {    
+                    if (counter < 10) {
                         System.out.println("0" + counter + " " + aFile.getFileName());
                         counter++;
-                    }
-                    else {
+                    } else {
                         System.out.println(counter + " " + aFile.getFileName());
                         counter++;
                     }
 
                 }
-              //if the system has issues finding the file or folder:
+                //if the system has issues finding the file or folder:
             } catch (IOException e) { //stream is automatically closed by now.
                 System.out.println("Some error in accessing files.");
             }
         }
     }
+
+    public static void main(String[] args) {
+        accessFiles(args);
+    }
+}
