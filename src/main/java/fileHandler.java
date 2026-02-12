f6386ae70eb63bb01d46fd93abfa826993a2d100
Julia Chen <juliachen174@gmail.com>
2026-02-12 14:54:18 -0500

    file handler to class

---
+++ src/main/java/fileHandler.java
@@ -0,0 +1,31 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class fileHandler {
+    private String fileName;
+    private String fileContents;
+
+    public fileHandler(String fn) {
+        fileName = fn;
+    }
+
+    public String returnFileContents() {
+        String fc = "";
+        File currentFile = new File("data/"+fileName); //files are located a folder called 'data'
+        try(Scanner scanner = new Scanner(currentFile)) {
+            while (scanner.hasNextLine()) { //reads the file contents line by line
+                String line = scanner.nextLine();
+                fc += (line + "\n"); //concatenates the file contents to a string
+            }
+            fileContents = fc;
+            return fc;
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("An error occurred while trying to access " + fileName + ".");
+            e.printStackTrace();
+        }
+        fileContents = "";
+        return "";
+    }
+}
